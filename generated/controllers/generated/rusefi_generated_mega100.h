dd87d3ccf5b14ffdbba3f6cc684e6d8783ee6f3b
GitHub git update Action <action@github.com>
2026-06-04 00:58:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega100.h
+++ generated/controllers/generated/rusefi_generated_mega100.h
@@ -2189,7 +2189,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.06.03.mega100.509666727"
+#define TS_SIGNATURE "rusEFI main.2026.06.04.mega100.509666727"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
